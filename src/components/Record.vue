439d6c3fcae3e7ddadd16866866fe40b3e8cb4a1
Ruiuiuiuiui <blue830215@gmail.com>
2021-08-02 00:03:14 +0800

    Updated Record buttons

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,38 +1,18 @@
 <template>
   <h1>Record</h1>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+  <nav class="navbar navbar-light bg-light">
+    <form class="container-fluid justify-content-end">
+      <router-link class="nav-link" to="/record/dailyRecord"
+        ><button type="button" class="btn btn-outline-warning">
+          DailyRecord
+        </button></router-link
       >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left links -->
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item"></li>
-        </ul>
-        <!-- Left links -->
-        <div class="d-flex align-items-center">
-          <router-link class="nav-link" to="/record/dailyRecord"
-            ><button type="button" class="btn btn-outline-warning">
-              DailyRecord
-            </button></router-link
-          >
-          <router-link class="nav-link" to="/record/report"
-            ><button type="button" class="btn btn-outline-warning">
-              Report
-            </button></router-link
-          >
-        </div>
-      </div>
-    </div>
+      <router-link class="nav-link" to="/record/report"
+        ><button type="button" class="btn btn-outline-warning">
+          Report
+        </button></router-link
+      >
+    </form>
   </nav>
   <div>
     <router-view />
